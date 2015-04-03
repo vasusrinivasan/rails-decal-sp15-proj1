1c5b7346e88a6a67563ed873c88d2d2bc1918df9
Vasu Srinivasan <vasusrinvsn@gmail.com>
2015-04-03 01:41:12 -0700

    Pokemon Battling Added

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -9,10 +9,25 @@ class PokemonsController < ApplicationController
 	def damage
 		@pokemon_id = Pokemon.find(params[:id])
 		@pokemon_id.health -= 10
-		if @pokemon_id.health <= 0
-			@pokemon_id.destroy
+		@pokemon_id.save
+
+		@pokemon_name = Pokemon.find_by(name: params[:name])
+		@pokemon_name.level += 1
+		@pokemon_name.save
+		redirect_to @pokemon_id.trainer
+	end
+
+	def heal
+		@pokemon_id = Pokemon.find(params[:id])
+		if @pokemon_id.health == 100
+			flash[:error] = "Your pokemon is already at full health!"
+			redirect_to @pokemon_id.trainer
+		elsif @pokemon_id.health >= 90
+			@pokemon_id.health = 100
+			@pokemon_id.save
 			redirect_to @pokemon_id.trainer
 		else
+			@pokemon_id.health += 10
 			@pokemon_id.save
 			redirect_to @pokemon_id.trainer
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :trainers
   patch '/capture', to: 'pokemons#capture', as: 'capture'
   patch '/damage', to: 'pokemons#damage', as: 'damage'
+  patch '/heal', to: 'pokemons#heal', as: 'heal'
   get '/new', to: 'pokemons#new', as: 'new'
   post '/create', to: 'pokemons#create', as: 'create'
 
